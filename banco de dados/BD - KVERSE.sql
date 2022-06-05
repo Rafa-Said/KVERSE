2c92d2a95f73110bd4275ba149d335d9d61b0b45
= <=>
2022-06-05 17:17:09 -0300

    atualização de banco

--- banco de dados/BD - KVERSE.sql
+++ banco de dados/BD - KVERSE.sql
@@ -10,33 +10,45 @@ CREATE TABLE usuario (
     Cel CHAR(15),
     Email VARCHAR(45),
     Senha VARBINARY(100)
-)  AUTO_INCREMENT=10;  
+);  
+CREATE TABLE voto (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    qtd_dramas INT,
+    qtd_ep INT,
+    fk_usuario INT,
+    FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+); 
 
 select * from usuario;
+select * from voto;
 
-CREATE TABLE aviso (
+select count(qtd_dramas) QtddeDramas, count(qtd_ep) QtdeEp  from voto group by fk_usuario ;
+
+select Nome, count(qtd_dramas) as QtdDramas, count(qtd_ep) as QtdEpisódios 
+	from voto join usuario 
+		on fk_usuario = usuario.id group by fk_usuario;
+
+-- ESTAS TABELAS AINDA NÃO FORAM USADAS -- 
+
+/*
+CREATE TABLE voto (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    titulo VARCHAR(100),
-    descricao VARCHAR(150),
+    qtd_drama INT,
+    qtd_ep INT,
     fk_usuario INT,
     FOREIGN KEY (fk_usuario)
         REFERENCES usuario (id)
 );
-select * from aviso;
 
-CREATE TABLE voto (
+
+CREATE TABLE aviso (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    titulo INT,
-    titulo2 INT,
+    qtd_dramas VARCHAR(100),
+    qtd_ep VARCHAR(150),
     fk_usuario INT,
-    FOREIGN KEY (fk_usuario)
-        REFERENCES usuario (id)
-); 
-select * from voto;
-
-select Nome, count(titulo) as 'Qtd de Votos' from voto join usuario on fk_usuario = usuario.id group by fk_usuario; -- Qtd de votos por usuário
-
--- ESTÁ TABELA AINDA NÃO FOI USADA -- 
+    FOREIGN KEY (fk_usuario) REFERENCES usuario (id)
+);
+*/
 
 CREATE TABLE usuarionota ( -- ESTÁ TABELA É ASSOCIATIVA, OU SEJA TEM A FK DE DUAS OUTRAS TABELA E É PRIMARY KEY
     fk_usuario INT,
@@ -51,7 +63,7 @@ CREATE TABLE usuarionota ( -- ESTÁ TABELA É ASSOCIATIVA, OU SEJA TEM A FK DE D
 );
 
 SELECT 
-    Email, MD5(Senha)
+    Nome, Email, MD5(Senha)
 FROM
     usuario
 
