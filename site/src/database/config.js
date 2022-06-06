1c4b9e3ff6f1591aa8ac0191989a169f351f6a16
rafa-said <rafaela.dias@sptech.school>
2022-06-06 09:11:01 -0300

    Adicionando o gráfico

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "kverse",
-    user: "Ella",
-    password: "urubu100",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
